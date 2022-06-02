56b85022dd2fe92888643572c7e1cfa46f21ddf4
RomanDev135 <r.nazarov1998@mail.ru>
2022-06-02 11:48:21 +0300

    Изменение 2. Сделали поля в форме обязательными

--- resume_tests/src/parsing/forms.py
+++ resume_tests/src/parsing/forms.py
@@ -4,10 +4,10 @@ from resume_tests.src.parsing.models import City,Language
 
 
 class FindForm(forms.Form):
-    city = forms.ModelChoiceField(queryset=City.objects.all(), to_field_name='slug', required=False,
+    city = forms.ModelChoiceField(queryset=City.objects.all(), to_field_name='slug', required=True,
                                   widget=forms.Select(attrs={'class': 'form-control'}),
                                   label='Город')
-    language = forms.ModelChoiceField(queryset=Language.objects.all(), to_field_name='slug', required=False,
+    language = forms.ModelChoiceField(queryset=Language.objects.all(), to_field_name='slug', required=True,
                                       widget=forms.Select(attrs={'class': 'form-control'}),
                                       label='Специальность')
 
